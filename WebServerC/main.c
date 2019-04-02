d47a3008155744a0edb6f724a63a615147ed40fa
alaincruzcasanova <alain.cruzc@gmail.com>
2019-04-02 08:19:47 -0600

    Initial Commit

---
+++ WebServerC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  WebServerC
+//
+//  Created by Alain Cruz Casanova on 4/2/19.
+//  Copyright © 2019 Alain Cruz Casanova. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
